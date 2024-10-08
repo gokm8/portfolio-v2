e58d7ae053030209cbf32a63540effca5eefa02c
Gøkmen Øzbayir <107099606+gokm8@users.noreply.github.com>
2024-10-08 15:28:41 +0200

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <section className='py-24'>
+      <div className='container'>
+        <div className='text-3x1 font-bold'>Next JS Starter</div>
+      </div>
+    </section>
+  )
+}
+
+export default Home
---
+++ tailwind.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}'
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem'
+      }
+    }
+  },
+  plugins: []
+}
+export default config

9aa5baea32b95f71d8f4eaae865d7979b9420901
gokm8 <gouzb22@student.sdu.dk>
2024-10-10 07:35:27 +0200

    change success border and background in toast.tsx

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -32,7 +32,7 @@ const toastVariants = cva(
         default: 'border bg-background text-foreground',
         destructive:
           'destructive group border-destructive bg-destructive text-destructive-foreground',
-        success: 'group border-neutral-600 bg-neutral-900	 '
+        success: 'group border-green-800 bg-green-950	 '
       }
     },
     defaultVariants: {

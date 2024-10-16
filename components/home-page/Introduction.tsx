74c4dd78da28fe9186086dbbc9b13f1f38b6519d
gokm8 <gouzb22@student.sdu.dk>
2024-10-16 18:53:18 +0200

    add border to intro section

--- components/home-page/Introduction.tsx
+++ components/home-page/Introduction.tsx
@@ -44,60 +44,62 @@ function Introduction() {
 
   return (
     <Container>
-      <div className='flex flex-row items-center'>
-        <div className='relative'>
-          {/* RippleLoader placed behind avatar */}
-          <RippleLoader className='absolute inset-0 z-0' />
+      <div className='rounded-md border border-neutral-800 p-4'>
+        <div className='flex flex-row items-center'>
+          <div className='relative'>
+            {/* RippleLoader placed behind avatar */}
+            <RippleLoader className='absolute inset-0 z-0' />
 
-          {/* avatar placed in front */}
-          <Avatar className='relative z-10 h-12 w-12'>
-            <AvatarImage src='https://avatars.githubusercontent.com/u/107099606?v=4' />
-            <AvatarFallback>GØ</AvatarFallback>
-          </Avatar>
+            {/* avatar placed in front */}
+            <Avatar className='relative z-10 h-12 w-12'>
+              <AvatarImage src='https://avatars.githubusercontent.com/u/107099606?v=4' />
+              <AvatarFallback>GØ</AvatarFallback>
+            </Avatar>
+          </div>
+
+          {/* name + passion */}
+          <div className='ml-4 flex flex-col space-y-1'>
+            <h2 className='text-xl font-semibold text-neutral-100'>
+              Gøkmen Øzbayir
+            </h2>
+            <p className='text-sm text-neutral-300'>
+              I&rsquo;m a software engineer who loves to build things.
+            </p>
+          </div>
         </div>
 
-        {/* name + passion */}
-        <div className='ml-4 flex flex-col space-y-1'>
-          <h2 className='text-xl font-semibold text-neutral-100'>
-            Gøkmen Øzbayir
-          </h2>
-          <p className='text-sm text-neutral-300'>
-            I&rsquo;m a software engineer who loves to build things.
+        {/* description + icons */}
+        <div className='mt-4 flex items-center justify-between'>
+          {/* description */}
+          <p className='text-xs leading-none text-neutral-500'>
+            Any inquiries? Email me @ gozbayir@hotmail.com
           </p>
-        </div>
-      </div>
 
-      {/* description + icons */}
-      <div className='mt-4 flex items-center justify-between'>
-        {/* description */}
-        <p className='text-xs leading-none text-neutral-500'>
-          Any inquiries? Email me @ gozbayir@hotmail.com
-        </p>
-
-        {/* icons */}
-        <div className='flex items-center space-x-4'>
-          <a
-            href='https://www.linkedin.com/in/gokmenozbayir/'
-            target='_blank'
-            rel='noopener noreferrer'
-            className='cursor-pointer text-neutral-100 hover:text-neutral-300'
-          >
-            <FaLinkedin size={20} />
-          </a>
-          <a
-            href='https://github.com/gokm8'
-            target='_blank'
-            rel='noopener noreferrer'
-            className='cursor-pointer text-neutral-100 hover:text-neutral-300'
-          >
-            <FaGithub size={20} />
-          </a>
-          <a
-            className='cursor-pointer text-neutral-100 hover:text-neutral-300'
-            onClick={() => setIsClicked(true)}
-          >
-            <MdEmail size={24} />
-          </a>
+          {/* icons */}
+          <div className='flex items-center space-x-4'>
+            <a
+              href='https://www.linkedin.com/in/gokmenozbayir/'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='cursor-pointer text-neutral-100 hover:text-neutral-300'
+            >
+              <FaLinkedin size={20} />
+            </a>
+            <a
+              href='https://github.com/gokm8'
+              target='_blank'
+              rel='noopener noreferrer'
+              className='cursor-pointer text-neutral-100 hover:text-neutral-300'
+            >
+              <FaGithub size={20} />
+            </a>
+            <a
+              className='cursor-pointer text-neutral-100 hover:text-neutral-300'
+              onClick={() => setIsClicked(true)}
+            >
+              <MdEmail size={24} />
+            </a>
+          </div>
         </div>
       </div>
     </Container>

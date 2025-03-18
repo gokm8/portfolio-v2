2901095a7e0513c0029c92a6cf0c33db295ca60d
gokm8 <gouzb22@student.sdu.dk>
2025-03-18 23:27:09 +0100

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { Layout } from 'components/craft'
 import { Toaster } from 'components/ui/toaster'
 // find the font at https://fonts.google.com/specimen/Poppins
 import { Poppins } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react'
 
 // variable of the font
 const poppins = Poppins({
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body className={`${poppins.variable}`}>
         {children}
         <Toaster />
+        <Analytics />
       </body>
     </Layout>
   )

0b6c5843b2b3b999c661441ed93a847c2cf9978f
gokm8 <gouzb22@student.sdu.dk>
2025-09-26 13:04:37 +0200

    update footer

--- components/new-home-page/wrapper/Footer.tsx
+++ components/new-home-page/wrapper/Footer.tsx
@@ -2,13 +2,15 @@ import { Container, Section } from '@/components/ds'
 
 function Footer() {
   return (
-    <Section className='bg-background'>
-      <Container>
-        <p className='text-caption'>
-          © {new Date().getFullYear()} Gøkmen Øzbayir
-        </p>
-      </Container>
-    </Section>
+    <footer className='bg-background'>
+      <Section>
+        <Container>
+          <p className='text-caption'>
+            © {new Date().getFullYear()} Gøkmen Øzbayir
+          </p>
+        </Container>
+      </Section>
+    </footer>
   )
 }
 

599fc1e0e785d0eceb1f57eb807249c6a21243f0
gokm8 <gouzb22@student.sdu.dk>
2025-09-30 22:49:58 +0200

    fix tracing beam length of following

--- components/ui/tracing-beam.tsx
+++ components/ui/tracing-beam.tsx
@@ -32,7 +32,7 @@ export const TracingBeam = ({
   }, [])
 
   const y1 = useSpring(
-    useTransform(scrollYProgress, [0, 0.8], [50, svgHeight]),
+    useTransform(scrollYProgress, [0, 0.9], [50, svgHeight]),
     {
       stiffness: 500,
       damping: 90

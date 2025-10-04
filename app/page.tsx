6ec32f8a5c5fcf8a999179a5445375550156325e
gokm8 <gouzb22@student.sdu.dk>
2025-10-04 13:51:53 +0200

    remove unused vars

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,6 @@
 import IntroductionNew from '@/components/new-home-page/Introduction'
 import CompletedProjectsNew from '@/components/new-home-page/CompletedProjects'
 import EducationNew from '@/components/new-home-page/Education'
-
-import Introduction from '@/components/home-page/Introduction'
-
-import Education from 'components/home-page/Education'
-import Technologies from 'components/home-page/Technologies'
-import FeaturedProjects from 'components/home-page/FeaturedProjects'
-import CurrentProjects from 'components/home-page/CurrentProjects'
-import CompletedProjects from 'components/home-page/CompletedProjects'
 import { Main } from 'components/ds'
 
 import React from 'react'

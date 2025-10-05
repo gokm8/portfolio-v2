e2a555c87871b374778f3607baba1e4be1fc5214
gokm8 <gouzb22@student.sdu.dk>
2025-10-06 00:34:10 +0200

    fix sparkles bug

--- components/ui/sparkles.tsx
+++ components/ui/sparkles.tsx
@@ -1,6 +1,5 @@
-// @ts-nocheck
 'use client'
-import React, { useId, useMemo } from 'react'
+import React, { useId } from 'react'
 import { useEffect, useState } from 'react'
 import Particles, { initParticlesEngine } from '@tsparticles/react'
 import type { Container, SingleOrMultiple } from '@tsparticles/engine'
@@ -81,7 +80,9 @@ export const SparklesCore = (props: ParticlesProps) => {
                   enable: false,
                   mode: 'repulse'
                 },
-                resize: true as any
+                resize: {
+                  enable: true
+                }
               },
               modes: {
                 push: {

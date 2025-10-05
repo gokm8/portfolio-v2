3536ecc41351549f79b382dd5313d861357af00f
gokm8 <gouzb22@student.sdu.dk>
2025-10-06 01:38:23 +0200

    change color of tracing beam

--- components/ui/tracing-beam.tsx
+++ components/ui/tracing-beam.tsx
@@ -76,7 +76,7 @@ export const TracingBeam = ({
               borderColor:
                 scrollYProgress.get() > 0 ? 'white' : 'var(--primary)'
             }}
-            className='h-2 w-2 rounded-full border border-neutral-300 bg-white'
+            className='bg-primary border-primary/50 h-2 w-2 rounded-full border'
           />
         </motion.div>
         <svg
@@ -89,7 +89,7 @@ export const TracingBeam = ({
           <motion.path
             d={`M 1 0V -36 l 18 24 V ${svgHeight * 0.8} l -18 24V ${svgHeight}`}
             fill='none'
-            stroke='#9091A0'
+            stroke='oklch(0.705 0.213 47.604 / 0.16)'
             strokeOpacity='0.16'
             transition={{
               duration: 10
@@ -114,10 +114,17 @@ export const TracingBeam = ({
               y1={y1} // set y1 for gradient
               y2={y2} // set y2 for gradient
             >
-              <stop stopColor='#18CCFC' stopOpacity='0'></stop>
-              <stop stopColor='#18CCFC'></stop>
-              <stop offset='0.325' stopColor='#6344F5'></stop>
-              <stop offset='1' stopColor='#AE48FF' stopOpacity='0'></stop>
+              <stop
+                stopColor='oklch(0.705 0.213 47.604)'
+                stopOpacity='0'
+              ></stop>
+              <stop stopColor='oklch(0.705 0.213 47.604)'></stop>
+              <stop offset='0.325' stopColor='oklch(0.646 0.222 41.116)'></stop>
+              <stop
+                offset='1'
+                stopColor='oklch(0.828 0.189 84.429)'
+                stopOpacity='0'
+              ></stop>
             </motion.linearGradient>
           </defs>
         </svg>

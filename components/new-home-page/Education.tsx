39113fdcbb85a117e34b8729e6e06508441e65b1
gokm8 <gouzb22@student.sdu.dk>
2025-10-06 22:04:18 +0200

    adding evarvault in the corners in the education section

--- components/new-home-page/Education.tsx
+++ components/new-home-page/Education.tsx
@@ -3,6 +3,7 @@ import { Container } from '../ds'
 import { Card, CardContent } from '../ui/card'
 import { Separator } from '../ui/separator'
 import { educationalData } from 'data/education'
+import { EvervaultCard, Icon } from '../ui/evervault-card'
 
 function EducationNew() {
   return (
@@ -12,7 +13,23 @@ function EducationNew() {
         <Separator orientation='horizontal' />
 
         {educationalData.map((data) => (
-          <Card key={data.id} className='mb-4 last:mb-0'>
+          <Card key={data.id} className='relative mt-4 mb-8 last:mb-0'>
+            <Icon
+              className='text-muted-foreground absolute -top-3 -left-3 h-6 w-6'
+              opacity={0.75}
+            />
+            <Icon
+              className='text-muted-foreground absolute -bottom-3 -left-3 h-6 w-6'
+              opacity={0.75}
+            />
+            <Icon
+              className='text-muted-foreground absolute -top-3 -right-3 h-6 w-6'
+              opacity={0.75}
+            />
+            <Icon
+              className='text-muted-foreground absolute -right-3 -bottom-3 h-6 w-6'
+              opacity={0.75}
+            />
             <CardContent>
               <div className='mb-1 flex flex-row justify-between'>
                 <h3 className='text-medium text-muted-foreground flex-shrink-0 text-sm'>

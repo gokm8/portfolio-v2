7e760aed47574b1f838e5d225fccc6e402f69145
gokm8 <gouzb22@student.sdu.dk>
2025-10-21 21:27:41 +0200

    fixing navbar

--- components/new-home-page/wrapper/Navbar.tsx
+++ components/new-home-page/wrapper/Navbar.tsx
@@ -5,28 +5,26 @@ import { Separator } from '@/components/ui/separator'
 
 export function Navigation() {
   return (
-    <Nav>
-      <Container>
-        <div className='flex items-center justify-between'>
-          <Link href='/' className='text-muted-foreground hover:text-primary'>
-            gokm8.xyz
-          </Link>
-          <ul className='flex items-center gap-4'>
-            <li>
-              <Link href='/' className='hover:text-primary'>
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link href='/blog' className='hover:text-primary'>
-                Projects
-              </Link>
-            </li>
-            <ModeToggle />
-          </ul>
-        </div>
-        <Separator />
-      </Container>
+    <Nav containerClassName='max-w-3xl'>
+      <div className='flex items-center justify-between'>
+        <Link href='/' className='text-muted-foreground hover:text-primary'>
+          gokm8.xyz
+        </Link>
+        <ul className='flex items-center gap-4'>
+          <li>
+            <Link href='/' className='hover:text-primary'>
+              Home
+            </Link>
+          </li>
+          <li>
+            <Link href='/blog' className='hover:text-primary'>
+              Projects
+            </Link>
+          </li>
+          <ModeToggle />
+        </ul>
+      </div>
+      <Separator />
     </Nav>
   )
 }
